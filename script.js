32c063e27a33eec415da5651fc0a33087b9b21e9
AMfaraz <67388525+AMfaraz@users.noreply.github.com>
2021-02-06 18:04:04 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,40 @@
+let count=0
+let arr=new Array(9).fill(null)
+
+//writing X or O
+function change(){
+  if(count%2==0){
+    this.innerText="X"
+    count=count+1
+    arr[this.value]=true
+    name="O"
+  }
+  else{
+    this.innerText="O"
+    count=count+1
+    arr[this.value]=false
+    name="X"
+  }
+  
+}
+//selection the buttons
+let drli1=document.querySelectorAll("button.r1")
+let drli2=document.querySelectorAll("button.r2")
+let drli3=document.querySelectorAll("button.r3")
+//adding events here
+for(let elem of drli1){
+  elem.addEventListener("click",change)
+ 
+}
+for(let elem of drli2){
+  elem.addEventListener("click",change)
+ 
+}
+for(let elem of drli3){
+  elem.addEventListener("click",change)
+ 
+}
+
+//conditions for a win
+
+
