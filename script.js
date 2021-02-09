cd5cacf503ad39855d436342c81e93f507d1cb46
AMfaraz <67388525+AMfaraz@users.noreply.github.com>
2021-02-09 23:03:26 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let count=0
 let arr=new Array(9).fill(null)
+let player1=prompt("Enter Your name!")
+let player2=prompt("Enter Your name!")
+
 
 //writing X or O
 function change(){
@@ -25,7 +28,6 @@ let drli3=document.querySelectorAll("button.r3")
 for(let elem of drli1){
   elem.addEventListener("click",change)
   elem.addEventListener("click",check)
- 
 }
 for(let elem of drli2){
   elem.addEventListener("click",change)
@@ -36,8 +38,7 @@ for(let elem of drli3){
   elem.addEventListener("click",check)
 }
 
-//conditions for a win
-
+//conditions for a win or draw
 function check(){
   if(arr[0]===true && arr[1]===true && arr[2]===true ||
      arr[3]===true && arr[4]===true && arr[5]===true ||
@@ -47,11 +48,21 @@ function check(){
      arr[2]===true && arr[5]===true && arr[8]===true ||
      arr[0]===true && arr[4]===true && arr[8]===true ||
      arr[2]===true && arr[4]===true && arr[6]===true ){
+
        setTimeout(()=>{
-         alert(`${this.innerText} won`)
-         reset()},50)
-      
+         if(this.innerText==="X"){
+          alert(`${player1} won`)
+          reset()
+         }
+
+         else{
+          alert(`${player2} won`)
+          reset()
+         }
+       },50)
+       
      }
+
   else if(arr[0]===false && arr[1]===false && arr[2]===false ||
      arr[3]===false && arr[4]===false && arr[5]===false ||
      arr[6]===false && arr[7]===false && arr[8]===false ||
@@ -61,9 +72,40 @@ function check(){
      arr[0]===false && arr[4]===false && arr[8]===false ||
      arr[0]===false && arr[4]===false && arr[8]===false ||
      arr[2]===false && arr[4]===false && arr[6]===false ){
+
        setTimeout(()=>{
-         alert(`${this.innerText} won`)
-         reset()},10)
+         if(this.innerText==="X"){
+          alert(`${player1} won`)
+          reset()
+         }
+
+         else{
+          alert(`${player2} won`)
+          reset()
+         }
+       },50)
+
      }
-  
+  else if(count>=9){
+      setTimeout(()=>{
+         alert(`It was a DRAW!`)
+         reset()},10)
+  }
+}
+
+//reset the game
+function reset(){
+  for(let elem of drli1){
+  elem.innerText=""
+}
+for(let elem of drli2){
+  elem.innerText=""
+}
+for(let elem of drli3){
+  elem.innerText=""
+}
+for(let i=0;i<9;i++){
+  arr[i]=null
+}
+count=0
 }
